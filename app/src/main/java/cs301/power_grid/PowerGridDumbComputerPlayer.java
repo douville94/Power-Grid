0a1f5dc3f4c058a17110e697df4b2d79bcf233ea
Luke Douville <luke.douville@gmail.com>
2017-04-20 18:44:40 -0700

    Removed all commented code from PowerGridDumbComputerPlayer.java.  PowerGridHumanPlayer.java still broken.

--- app/src/main/java/cs301/power_grid/PowerGridDumbComputerPlayer.java
+++ app/src/main/java/cs301/power_grid/PowerGridDumbComputerPlayer.java
@@ -15,19 +15,13 @@ import game.infoMsg.GameInfo;
 
 public class PowerGridDumbComputerPlayer extends GameComputerPlayer {
     private GameMainActivity activity;
-//    PowerState powerState;
     PowerState powerState = new PowerState();
-//    PowerState powerState = new PowerState(PowerGridDumbComputerPlayer.this.powerState);
-//    PowerGridHumanPlayer instanceVariables = new PowerGridHumanPlayer("DumbAI");
-//    private GamePlayer tempPlayer;
     private int selectNum = -1;
     private int bidValue = -1;
     private ResourceStore localStore = new ResourceStore();
-//    bidValue = Integer.parseInt(bidVal.getText().toString());
-    private Powerplant tempPlant;// = new Powerplant();
-    private Powerplant selectedPowerPlant;// = new Powerplant();
+    private Powerplant tempPlant;
+    private Powerplant selectedPowerPlant;
     private boolean isClicked = false;
-//    private GamePlayer tempPlayer;
     /**
      * constructor
      *
@@ -38,39 +32,23 @@ public class PowerGridDumbComputerPlayer extends GameComputerPlayer {
     }
 
     public void receiveInfo(GameInfo info) {
-//        Powerplant selectedPowerPlant;// = new Powerplant(selectedPowerPlant.getCost(), selectedPowerPlant.getPtp(), selectedPowerPlant.getHp(), selectedPowerPlant.getKind());
-        //check if info is a gameState
+        /*Check if info is a GameState object.*/
         if (info instanceof PowerState) {
             powerState = (PowerState)info;
-            //look at what phase game is in then make a move
+            /*Determine the phase of the game before making a move.*/
             int phase = powerState.getGamePhase();
             /*Make sure the dumb AI has enough money to buy anything.*/
             if (phase == 0) {
                 /*First player chooses a power plant.
                 * "OK" or "Pass" updates phase.*/
-
                 /*Use a random integer because this is the dumb AI.*/
                 int randOKPass = (int)(Math.random() * 2);
-//                int randPPlantToBidOnIndex = (int)(Math.random() * 42);
                 int randPPlantToBidOnIndex = (int)(Math.random() * 4);
-//                int rand1 = (int)(Math.random() * 4);
-
-//                powerState.initiatePowerPlants();
-//                selectedPowerPlant = powerState.getAvailPowerplant().get(randPPlantToBidOnIndex);
-
-
-//                isClicked = true;
-//                if(isClicked == true) {
                 if(randOKPass == 0) {
-                    phase = 1;
-//                    UpdatePhaseAction upa = new UpdatePhaseAction(PowerGridDumbComputerPlayer.this, phase);
-//                    game.sendAction(upa);
                     PassAction pa = new PassAction(PowerGridDumbComputerPlayer.this);
                     game.sendAction(pa);
-//                    return;
                 }
                 else {
-//                    SelectPowerPlantAction sppa = new SelectPowerPlantAction(PowerGridDumbComputerPlayer.this, selectedPowerPlant);
                     SelectPowerPlantAction sppa = new SelectPowerPlantAction(PowerGridDumbComputerPlayer.this, selectNum);
                     game.sendAction(sppa);
                 }
@@ -80,26 +58,12 @@ public class PowerGridDumbComputerPlayer extends GameComputerPlayer {
                 /*Bidding on power plant(s).
                 * Bidding or "pass" updates phase.*/
                 BidAction bid;
-//                BidAction bid = new BidAction(this, (int)Math.random()*20);
-//                bid.getBid();
-//                bidVal.setOnKeyListener(new bidEditTextListener());
                 int randOKPass = (int)(Math.random()*2);
-//                isClicked = true;
                 /*You could just put this in an else statement after evaluating randOKPass below.*/
-//                if(isClicked == true) {
-//                    phase = 2;
-//                    UpdatePhaseAction upa = new UpdatePhaseAction(PowerGridDumbComputerPlayer.this, phase);
-//                    game.sendAction(upa);
-//                    return;
-//                }
                 if(randOKPass == 0) {
-//                    phase = 2;
                     bid = new BidAction(this, (int)Math.random()*20);
                     bid.getBid();
                     game.sendAction(bid);
-//                    UpdatePhaseAction upa = new UpdatePhaseAction(PowerGridDumbComputerPlayer.this, phase);
-//                    game.sendAction(upa);
-//                    return;
                 }
                 else if(randOKPass == 1) {
                     /*pass*/
@@ -115,133 +79,29 @@ public class PowerGridDumbComputerPlayer extends GameComputerPlayer {
                 * We don't determine who the previous passer was in this if statement.*/
                 int randOKPass = (int)(Math.random() * 2);
                 int randPPlantToBidOn = (int)(Math.random() * 42);
-//                selectedPowerPlant.setCost(powerState.getAvailPowerplant().get(randPPlantToBidOn).getCost());
-//                selectedPowerPlant.setPtP(powerState.getAvailPowerplant().get(randPPlantToBidOn).getPtP());
-//                selectedPowerPlant.setHp(powerState.getAvailPowerplant().get(randPPlantToBidOn).getHp());
-//                selectedPowerPlant.setKind(powerState.getAvailPowerplant().get(randPPlantToBidOn).getKind());
-
-//                isClicked = true;
                 /*You could just put this in an else statement after evaluating randOKPass below.*/
-//                if(isClicked == true) {
-//                    phase = 3;
-//                    UpdatePhaseAction upa = new UpdatePhaseAction(PowerGridDumbComputerPlayer.this, phase);
-//                    game.sendAction(upa);
                 PassAction pa = new PassAction(PowerGridDumbComputerPlayer.this);
                 game.sendAction(pa);
-//                    return;
-//                }
                 if(randOKPass == 1) {
-//                    phase = 3;
-//                    UpdatePhaseAction upa = new UpdatePhaseAction(PowerGridDumbComputerPlayer.this, phase);
-//                    game.sendAction(upa);
-//                    SelectPowerPlantAction sppa = new SelectPowerPlantAction(PowerGridDumbComputerPlayer.this, selectedPowerPlant);
                     SelectPowerPlantAction sppa = new SelectPowerPlantAction(PowerGridDumbComputerPlayer.this, selectNum);
                     game.sendAction(sppa);
-
-//                    return;
                 }
                 /*Pass*/
             } else if(phase == 3) {
                 /*Second player chooses resources.
                 * "OK" or "Pass" updates phase.*/
-
                 /*Unnecessary since you're just making it pass.*/
-//                int randOKPass = (int)(Math.random() * 2);
-
-//                isClicked = true;
                 /*You could just put this in an else statement after evaluating randOKPass below.*/
-//                if (isClicked == true) {
-//                    phase = 4;
-                    UpdatePhaseAction upa = new UpdatePhaseAction(PowerGridDumbComputerPlayer.this, phase);
-                    game.sendAction(upa);
-//                    return;
-//                }
-//                if (randOKPass == 1) {
-//                    phase = 4;
-//                    SelectPowerPlantAction sppa = new SelectPowerPlantAction(PowerGridDumbComputerPlayer.this, selectedPowerPlant);
-//                    game.sendAction(sppa);
-//                    UpdatePhaseAction upa = new UpdatePhaseAction(PowerGridDumbComputerPlayer.this, phase);
-//                    game.sendAction(upa);
-//                    return;
-//                }
+                UpdatePhaseAction upa = new UpdatePhaseAction(PowerGridDumbComputerPlayer.this, phase);
+                game.sendAction(upa);
 
-//                    for (int i = 0; i < 15; i++) {
-//                        //if it's not available in the local store, it must have been bought!
-//                        if (!localStore.coal[i]) {
-//                            BuyCoalAction bca = new BuyCoalAction(PowerGridDumbComputerPlayer.this, i);
-//                            game.sendAction(bca);
-//                            return;
-//                        }
-//
-//                        if (!localStore.trash[i]) {
-//                            BuyTrashAction bta = new BuyTrashAction(PowerGridDumbComputerPlayer.this, i);
-//                            game.sendAction(bta);
-//                            return;
-//                        }
-//
-//                        if (i < 5 && !localStore.uranium[i]) {
-//                            BuyUraniumAction bua = new BuyUraniumAction(PowerGridDumbComputerPlayer.this, i);
-//                            game.sendAction(bua);
-//                            return;
-//                        }
-//
-//                        if (i < 10 && !localStore.oil[i]) {
-//                            BuyOilAction boa = new BuyOilAction(PowerGridDumbComputerPlayer.this, i);
-//                            game.sendAction(boa);
-//                            return;
-//                        }
-                        PassAction pa = new PassAction(PowerGridDumbComputerPlayer.this);//modify sos that it doesn't require powerplant
-//                    }
-//                }
+                PassAction pa = new PassAction(PowerGridDumbComputerPlayer.this);
             /*Pass*/
             } else if(phase == 4) {
                 /*First player chooses resources.
                 * "OK" or "Pass" updates phase.*/
 
-                /*Unnecessary since you're just making it pass.*/
-//                int randOKPass = (int)(Math.random() * 2);
-
-//                isClicked = true;
-                /*You could just put this in an else statement after evaluating randOKPass below.*/
-//                if (isClicked == true) {
-//                    phase = 4;
-//                    UpdatePhaseAction upa = new UpdatePhaseAction(PowerGridDumbComputerPlayer.this, phase);
-//                    game.sendAction(upa);
-//                    return;
-//                }
-//                if (randOKPass == 1) {
-//                    phase = 4;
-//                    SelectPowerPlantAction sppa = new SelectPowerPlantAction(PowerGridDumbComputerPlayer.this, selectedPowerPlant);
-//                    game.sendAction(sppa);
-//                    UpdatePhaseAction upa = new UpdatePhaseAction(PowerGridDumbComputerPlayer.this, phase);
-//                    game.sendAction(upa);
-//                    return;
-
-//                    for(int i = 0; i < 15; i++) {
-//                        //if it's not available in the local store, it must have been bought!
-//                        if (!localStore.coal[i]) {
-//                            BuyCoalAction bca = new BuyCoalAction(PowerGridDumbComputerPlayer.this, i);
-//                            game.sendAction(bca);
-//                        }
-//                        if (!localStore.trash[i]) {
-//                            BuyTrashAction bta = new BuyTrashAction(PowerGridDumbComputerPlayer.this, i);
-//                            game.sendAction(bta);
-//                        }
-//                        if (i < 5) {
-//                            if (!localStore.uranium[i]) {
-//                                BuyUraniumAction bua = new BuyUraniumAction(PowerGridDumbComputerPlayer.this, i);
-//                                game.sendAction(bua);
-//                            }
-//                        }
-//                        if (i < 10) {
-//                            if (!localStore.oil[i]) {
-//                                BuyOilAction boa = new BuyOilAction(PowerGridDumbComputerPlayer.this, i);
-//                                game.sendAction(boa);
-//                            }
-//                        }
-//                    }
                 PassAction pa = new PassAction(PowerGridDumbComputerPlayer.this);
-//                }
                 /*Pass*/
             } else if(phase == 5) {
                 /*Second player chooses cities.
@@ -251,17 +111,13 @@ public class PowerGridDumbComputerPlayer extends GameComputerPlayer {
                 selectedCity = powerState.getAvailCities().get(randCityToBidOnIndex);
                 int randOKPass = (int)(Math.random() * 2);
 
-//                isClicked = true;
-//                if(isClicked == true) {
-                    phase = 6;
-                    UpdatePhaseAction upa = new UpdatePhaseAction(PowerGridDumbComputerPlayer.this, phase);
-                    game.sendAction(upa);
-//                }
-//                if(randOKPass == 1) {
-                    BuyCityAction bca = new BuyCityAction(PowerGridDumbComputerPlayer.this, selectedCity);
-                    game.sendAction(bca);
-//                    return;
-//                }
+
+                phase = 6;
+                UpdatePhaseAction upa = new UpdatePhaseAction(PowerGridDumbComputerPlayer.this, phase);
+                game.sendAction(upa);
+
+                BuyCityAction bca = new BuyCityAction(PowerGridDumbComputerPlayer.this, selectedCity);
+                game.sendAction(bca);
             /*Pass*/
             } else if(phase == 6) {
                 /*First player chooses cities.
